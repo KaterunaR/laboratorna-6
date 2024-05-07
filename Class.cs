375a82657e9a57705f4f3e506688644dacdeef7e
KaterunaR <131966803+KaterunaR@users.noreply.github.com>
2024-05-07 19:24:07 +0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_lab6
+{
+    public class Car : IComparable<Car>
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Dealer { get; set; }
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Ціна не може бути від'ємною.");
+                price = value;
+            }
+        }
+
+        public Car()
+        {
+        }
+
+        public Car(string brand, string model, int year, string dealer, double price)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Dealer = dealer;
+            Price = price;
+        }
+
+
+        public void DecreasePriceForFordLastYear(int currentYear)
+        {
+            if (Brand == "Ford" && Year == currentYear - 1)
+            {
+                Price *= 0.8; 
+            }
+        }
+        public int CompareTo(Car other)
+        {
+            return string.Compare(Dealer, other.Dealer);
+        }
+    }
+
+    public class CarCollection
+    {
+        private List<Car> cars = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+
+        public void DisplayCars()
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Brand} {car.Model}, {car.Year}, {car.Dealer}, {car.Price}$");
+            }
+        }
+
+
+        public void SortByDealer()
+        {
+            cars.Sort();
+        }
+
+
+        // Запис колекції автомобілів у файл
+        public void WriteToFile(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (var car in cars)
+                {
+                    writer.WriteLine($"{car.Brand},{car.Model},{car.Year},{car.Dealer},{car.Price}");
+                }
+            }
+        }
+
+        // Зчитування колекції автомобілів з файлу
+        public void ReadFromFile(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    Car car = new Car(parts[0], parts[1], int.Parse(parts[2]), parts[3], double.Parse(parts[4]));
+                    cars.Add(car);
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_lab6
+{
+    public partial class Form1 : Form
+    {
+        CarCollection collection = new CarCollection();
+        public Form1()
+        {
+            InitializeComponent();
+            collection.AddCar(new Car("Ford", "Focus", 2023, "Ford Dealer 1", 25000));
+            collection.AddCar(new Car("Toyota", "Camry", 2022, "Toyota Dealer", 30000));
+            collection.AddCar(new Car("Ford", "Fiesta", 2021, "Ford Dealer 2", 20000));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            collection.DisplayCars();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            collection.SortByDealer();
+            Console.WriteLine($"---------------Sorted data------------------------$");
+            collection.DisplayCars();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            collection.WriteToFile(@"D:\obdz 2024\oop_lab6\oop_lab6\cars.txt");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            collection.ReadFromFile(@"D:\obdz 2024\oop_lab6\oop_lab6\cars.txt");
+            Console.WriteLine($"---------------Data was loaded------------------------$");
+        }
+    }
+}
